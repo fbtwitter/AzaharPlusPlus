c40a192ae98088a54a870a1b4fd1f35260386c71
Reza F. Augusdi <rzaugusdi354@gmail.com>
2026-08-20 14:24:22 +0700

    Revert "core: restore AzaharPlus's no-3DS-required fallbacks and old3ds download"
    This reverts commit 4555bfc29dcee124a4e64f92c6b3c0335723e05e.

--- src/core/system_titles.h
+++ src/core/system_titles.h
@@ -13,12 +13,7 @@ namespace Core {
 
 constexpr u32 NUM_SYSTEM_TITLE_REGIONS = 7;
 
-enum SystemTitleSet : u32 {
-    Minimal = 1 << 0,
-    Old3ds = 1 << 1,
-    New3ds = 1 << 2,
-    OldAndNew = Old3ds | New3ds
-};
+enum SystemTitleSet : u32 { Minimal = 1 << 0, Old3ds = 1 << 1, New3ds = 1 << 2 };
 
 /// Returns a list of firmware title IDs for a specific set and region.
 std::vector<u64> GetSystemTitleIds(SystemTitleSet set, u32 region);
